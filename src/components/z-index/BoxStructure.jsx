15e20e0b617c4fe4b57de27a756a49f989ed2e33
2018eeb1139 <amanpratapchandra1999@gmail.com>
2026-02-18 13:13:34 +0530

    z-index wanderon CTO ques

---
+++ src/components/z-index/BoxStructure.jsx
@@ -0,0 +1,46 @@
+import "../styles.css";
+
+export default function BoxStructure() {
+  return (
+    <div
+      style={{ zIndex: 1, backgroundColor: "magenta" }}
+      className="allDiv"
+      data-name="A"
+    >
+      {/* <div
+        style={{ zIndex: 2, backgroundColor: "blue" }}
+        className="allDiv"
+        data-name="B"
+      > */}
+      {/* <div
+          style={{ zIndex: 1, backgroundColor: "green" }}
+          className="allDiv"
+          data-name="C"
+        /> */}
+      {/* <div
+          style={{ zIndex: 5, backgroundColor: "orange" }}
+          className="allDiv"
+          data-name="D"
+        /> */}
+      {/* </div> */}
+      {/* <div
+        style={{ zIndex: 3, backgroundColor: "brown" }}
+        className="allDiv"
+        data-name="E"
+      > */}
+      {/* <div
+          style={{ zIndex: 2, backgroundColor: "cyan" }}
+          className="allDiv"
+          data-name="F"
+        /> */}
+      {/* <div
+          style={{ zIndex: 5, backgroundColor: "yellow" }}
+          className="allDiv"
+          data-name="G"
+        /> */}
+      {/* </div> */}
+      {/* GFEDCBA */}
+      {/* GFEDCBA */}
+    </div>
+  );
+}
